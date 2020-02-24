61fd60a01b11a8a01722c77dae6c4e31174915f0
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-24 15:48:40 -0500

    agregando componente resource que mapea endpoints, us ode ListGuesser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // in src/App.js
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-/* El componente admin es el componente raiz, este espera un prop dataProvider
-   que es capaz de recuperar (fetching) datos de una API */
-const App = () => <Admin dataProvider={dataProvider} />;
+/* El componente <Admin> espera uno o mas componentes hijos <Resource>,
+   cada resource mapea un nombre a un endpoint en al API */
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="albums" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
 
