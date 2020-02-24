2d3f3f45493a679b374e563005c2e112899c2f97
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-24 15:39:34 -0500

    agregando comentarios, para entender paso a paso las dependencias usadas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Admin } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+/* El componente admin es el componente raiz, este espera un prop dataProvider
+   que es capaz de recuperar (fetching) datos de una API */
 const App = () => <Admin dataProvider={dataProvider} />;
 
 export default App;
