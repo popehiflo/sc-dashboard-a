77e529b7e5c119f655d9912d5daae8cb32357b6c
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-24 17:29:40 -0500

    Agregando nuevo componente personalizado para un field del Datagrid. A este componente se le agrega estilos material-ui (jss)

--- src/users.js
+++ src/users.js
@@ -1,6 +1,7 @@
 // in src/users.js
 import React from 'react';
-import { List, Datagrid, TextField, EmailField, UrlField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import MyUrlField from './MyUrlField';
 
 export const UserList = props => (
     <List {...props}>
@@ -9,7 +10,7 @@ export const UserList = props => (
             <TextField source="name" />
             <EmailField source="email" />
             <TextField source="phone" />
-            <UrlField source="website" />
+            <MyUrlField source="website" />
             <TextField source="company.name" />
         </Datagrid>
     </List>
