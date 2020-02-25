daad2b8c1756f0d983f7e952ff2fa3dbd9a4e212
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-24 20:11:01 -0500

    Componentes para editar y crear post, en el simpleform de editar se quita el userId para no modificarlo. Se cambia el boton Editar por una accion explicita en el componente PostList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from "./users";
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 /* Componente personalizado, UserList */
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } />
     <Resource name="users" list={ UserList } />
   </Admin>
 );
---
+++ src/posts.js
@@ -0,0 +1,73 @@
+// in src/posts.js
+import React from 'react';
+import { List, Edit, Create,
+        Datagrid, 
+        TextField, 
+        ReferenceField,
+        EditButton,
+        ReferenceInput,
+        TextInput,
+        SelectInput,
+        SimpleForm,
+         } from 'react-admin';
+
+
+/*async UNSAFE_componentDidMount(){
+    await this.fetchPosts()
+}
+fetchExercises = async() => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data = await res.json();
+
+    this.setState({
+        data
+    })
+
+    console.log(data);
+}
+*/
+
+/* Listar los post que llegan desde la API */     
+export const PostList = props => (
+    <List {...props}>
+        {/*cambiamos esto <Datagrid rowClick="edit"> por un boton de accion explicita
+            lineas mas abajo */}
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            {/*Este campo body deberia aparecer en el DataGrid, sino en una vista detallada*/}
+            <TextField source="body" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+/* Editar un post */
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+               <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+/* Crear un nuevo post */
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+               <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
